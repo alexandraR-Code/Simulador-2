76749df45fcc3225b0d17fa81181aabd57c247ae
Alexandra Ramirez <alexa090114@gmail.com>
2026-05-04 18:30:15 -0500

    Parte 1 y 2: navegacion entre secciones y configurar tasa

--- simulador_completo.js
+++ simulador_completo.js
@@ -11,3 +11,29 @@
 
   
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+function ocultarSecciones(){
+  //Quitar la clase activa a todas las secciones por su id
+  document.getElementById("parametros").classList.remove("activa");
+  document.getElementById("clientes").classList.remove("activa");
+}
+//funcion que muestra solo la seccion cuyo id recibe como parametro 
+function mostrarSeccion(id){
+  //incovamos la funcion 
+  ocultarSecciones(); 
+  //Agregamos la clase activa solo a la seccion indicada
+  document.getElementById(id).classList.add("activa");
+}
+function guardarTasa(){
+  //obtener el valor input y convertirlo a numero
+  let tasa = recuperarFloat("tasaInteres");
+  //validamos que este entre el 10 y 20
+  if(tasa >= 10 && tasa <=20){
+    //si es valido, guaradamos la variable y mostramos mensaje exitoso
+    tasaInteres = tasa;
+    mostrarTexto("mensajeTasa", "Tasa configurada correctamente: " + tasa + "%");
+
+  }else{
+    // si no es valido mostramos mensaje de error 
+    mostrarTexto("mensajeTasa", "La tasa debe estar entre 10% y 20%");
+  }
+}
